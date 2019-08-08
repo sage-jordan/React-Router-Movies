69ab11462004b5b05c9458bda4e259520596eb81
sage-jordan <sagemjrodan@gmail.com>
2019-08-07 20:41:56 -0400

    Finished first stretch goal

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const MovieCard = props => {
-  return;
+  const { title, director, metascore, stars } = props;
+  return(
+    <div className="movie-card">
+      <Link to="/movies/:id">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </ Link>
+    </div>
+  )
 };
 
 export default MovieCard;
