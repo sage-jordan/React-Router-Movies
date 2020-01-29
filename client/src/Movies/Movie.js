52bd70b214a98c51b145f3e72a5fdf8e6b29138e
sage-jordan <sagemjordan@gmail.com>
2020-01-28 19:34:56 -0500

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -6,7 +6,6 @@ const Movie = props => {
 
   useEffect(() => {
     const id = props.match.params.id;
-    console.log(id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
