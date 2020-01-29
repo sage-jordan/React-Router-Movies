64e31cb895844c68113732f451c2332df92dbeab
sage-jordan <sagemjordan@gmail.com>
2020-01-28 20:20:26 -0500

    add cool face API

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cool = require("cool-ascii-faces");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/cool", (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 const movies = [
   {
     id: 0,
@@ -66,8 +74,4 @@ app.get("/api/movies/:id", (req, res) => {
 app.post("/api/movies", (req, res) => {
   if (req.body.id !== undefined) movies.push(req.body);
   res.status(201).json(movies);
-});
-
-app.listen(5000, () => {
-  console.log("Server listening on port 5000");
 });
