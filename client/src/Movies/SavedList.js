0c626cd60606f8824ca5fcfedd978392b267f1b7
sage-jordan <sagemjordan@gmail.com>
2020-01-28 19:49:42 -0500

    Save list has links to movie

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
+    {props.list.map(movie => {
+      const id = movie.id;
+      return (
+        <Link to={`/movies/${id}`}>
+          <span className="saved-movie">{movie.title}</span>
+        </Link>
+      );
+    })}
     <Link to="/">
       <div className="home-button">Home</div>
     </Link>
